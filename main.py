a796eca126cd0319d6433c5d4ba31ff2b1523c08
Artjoms8384 <77109398+Artjoms8384@users.noreply.github.com>
2021-12-02 07:37:32 +0000

    da

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-mystr = "trijsturis"
-myit = iter(mystr)
-
-print(next(myit))
-print(next(myit))
-print(next(myit))
-print(next(myit))
-print(next(myit))
-print(next(myit))
-print(next(myit))
-print(next(myit))
-print(next(myit))
-print(next(myit))
+my_string = "LAtvijas Republika"
+x = 0
+for i in my_string:
+  x = x + 1
+  print(my_string[0:x])
